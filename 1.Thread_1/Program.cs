abe5845af5eb44a1a476d776ce6cfe29ac0111f4
acalvo <acalvo@calrom.com>
2023-03-01 22:29:28 +0000

    First Thread

---
+++ 1.Thread_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Thread t = new Thread(dislplayMessage);
+
+t.Start();
+
+Console.WriteLine("Hello, World!");
+Thread.Sleep(1000); 
+Console.WriteLine("Hello, World!");
+Thread.Sleep(1000);
+Console.WriteLine("Hello, World!");
+Thread.Sleep(1000);
+Console.WriteLine("Hello, World!");
+Thread.Sleep(1000);
+Console.WriteLine("Hello, World!");
+Thread.Sleep(1000);
+Console.WriteLine("Hello, World!");
+Thread.Sleep(1000);
+Console.WriteLine("Hello, World!");
+
+void dislplayMessage ()
+{
+    Console.WriteLine("Hello, World! Thread_2");
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello, World! Thread_2");
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello, World! Thread_2");
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello, World! Thread_2");
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello, World! Thread_2");
+    Thread.Sleep(1000);
+    Console.WriteLine("Hello, World! Thread_2");
+
+}
